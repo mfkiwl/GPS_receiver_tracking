40dc0374973274b32b7487d427bfe004b8dc1f4b
some-repository <patch.code2002@gmail.com>
2025-05-12 08:52:33 +0300

    Changed loop filtes coefficients. Now tracking works.

--- DLL_loop_filter.m
+++ DLL_loop_filter.m
@@ -4,8 +4,8 @@ function shift = DLL_loop_filter (D_new, satellite_number, integrate_periods) % 
         % Execute this code for a single time to calculate filter
         % coefficients C_1 and C_2
         % K_0 * K_d = 1
-        zeta = 1; % damping ratio
-        B_L = 20; % noise bandwidth in Hz
+        zeta = 0.7; % damping ratio
+        B_L = 2; % noise bandwidth in Hz
 
         periods_per_second = 1000;
         %sample_rate = 5115000;
@@ -14,8 +14,11 @@ function shift = DLL_loop_filter (D_new, satellite_number, integrate_periods) % 
 
         omega_n = 8 * zeta * B_L / (4 * zeta^2 + 1); % loop filter natural frequency 
         
-        C_1 = 8 * zeta * omega_n * T / (4 + 4 * zeta * omega_n * T + (omega_n * T)^2);
-        C_2 = 4 * (omega_n * T)^2 / (4 + 4 * zeta * omega_n * T + (omega_n * T)^2);
+        %C_1 = 8 * zeta * omega_n * T / (4 + 4 * zeta * omega_n * T + (omega_n * T)^2);
+        %C_2 = 4 * (omega_n * T)^2 / (4 + 4 * zeta * omega_n * T + (omega_n * T)^2);
+
+        C_1 = (2 * zeta / omega_n) / (1 / (omega_n^2));
+        C_2 = T / (1 / (omega_n^2));
         fprintf (1, 'DLL: C_1 = %d, C_2 = %d\n', C_1, C_2);
 
         satellites_quantity = 31;
--- loop_filter.m
+++ loop_filter.m
@@ -4,8 +4,8 @@ function f = loop_filter (phi_new, satellite_number, integrate_periods) % Second
         % Execute this code for a single time to calculate filter
         % coefficients C_1 and C_2
         % K_0 * K_d = 1
-        zeta = 1; % damping ratio
-        B_L = 30; % noise bandwidth in Hz
+        zeta = 0.707; % damping ratio
+        B_L = 18; % noise bandwidth in Hz
 
         periods_per_second = 1000;
         %sample_rate = 5115000;
@@ -14,8 +14,11 @@ function f = loop_filter (phi_new, satellite_number, integrate_periods) % Second
 
         omega_n = 8 * zeta * B_L / (4 * zeta^2 + 1); % loop filter natural frequency 
         
-        C_1 = 8 * zeta * omega_n * T / (4 + 4 * zeta * omega_n * T + (omega_n * T)^2);
-        C_2 = 4 * (omega_n * T)^2 / (4 + 4 * zeta * omega_n * T + (omega_n * T)^2);
+        %C_1 = 8 * zeta * omega_n * T / (4 + 4 * zeta * omega_n * T + (omega_n * T)^2);
+        %C_2 = 4 * (omega_n * T)^2 / (4 + 4 * zeta * omega_n * T + (omega_n * T)^2);
+
+        C_1 = ((2 * zeta / omega_n) / (0.25 / (omega_n^2))) * (1 / (2 * pi));
+        C_2 = (T / (0.25 / (omega_n^2))) * (1 / (2 * pi));
         fprintf (1, 'PLL: C_1 = %d, C_2 = %d\n', C_1, C_2);
 
         satellites_quantity = 31;
